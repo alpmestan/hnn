e06e763d611dbac4cfc5e53d907c8f178a7b372d
Gatlin C Johnson <rokenrol@gmail.com>
2012-10-21 15:34:35 -0500

    changing the way this stuff works a bit

--- AI/HNN/Recurrent/Network.hs
+++ AI/HNN/Recurrent/Network.hs
@@ -13,7 +13,7 @@
 -- A network is an adjacency matrix of connection weights, the number of
 -- neurons, and the number of inputs.
 
-module AI.HNN.Recurrent.Network (Network, createNetwork, computeNetworkWith,
+module AI.HNN.Recurrent.Network (Network, createNetwork, computeStep,
                                  sigmoid) where
 
 import AI.HNN.Internal.Matrix
@@ -27,22 +27,29 @@ import System.Random.MWC
 -- | Our recurrent neural network
 data Network a = Network
                  { weights :: !(Matrix a)
+                 , state   :: !(Vec a)
                  , size    :: {-# UNPACK #-} !Int
                  , nInputs :: {-# UNPACK #-} !Int
                  } deriving Show
 
 -- | Creates a network with n neurons, m of which are inputs, and randomized weights
-createNetwork :: (Variate a, U.Unbox a) => Int -> Int -> IO (Network a)
+createNetwork :: (Variate a, U.Unbox a, Fractional a) => Int -> Int -> IO (Network a)
 createNetwork n m = withSystemRandom . asGenST $ \gen -> do
+    let ov = U.fromList (replicate n 0.0)
     rm <- uniformVector gen (n*n)
-    return $! Network (Matrix rm n n) n m
+    return $! Network (Matrix rm n n) ov n m
 
 -- | Evaluates a network with the specified function and list of inputs
 --   precisely one time step.
-computeNetworkWith :: (U.Unbox a, Num a) =>
-    Network a -> (a -> a) -> Vec a -> Vec a -> Vec a
-computeNetworkWith (Network{..}) activation thresh input =
-    U.map activation $! U.zipWith (-) (weights `apply` input) thresh
+--   Since we're in a Monad I think we can use mutable vectors. Look up later
+
+computeStep :: (U.Unbox a, Num a, Monad m) => Network a -> (a -> a) -> Vec a -> Vec a -> m (Network a)
+computeStep (Network{..}) activation thresh input = do
+    next <- return $ U.map activation $! U.zipWith (-) (weights `apply` state) thresh
+    return $ Network weights (overlay input next nInputs) size nInputs
+    where
+        overlay :: (M.Unbox a) => Vec a -> Vec a -> Int -> Vec a
+        overlay new old i = new U.++ (U.drop i old)
 
 sigmoid :: Floating a => a -> a
 sigmoid !x = 1 / (1 + exp (-x))
